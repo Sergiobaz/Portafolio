08d2726e80a57067400e306494a96520e4ea4edb
Sergiobaz <bazan.sergio2399@gmail.com>
2024-10-22 17:37:03 -0300

    details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,21 @@ const Footer = ({ languaje }) => {
   return (
     <div className=" w-full border-t grid ">
       <div className=" justify-self-center flex space-x-6 my-6">
-        <a href={LINKS.eMail} target="_blank">
+        {/* <a href={LINKS.eMail} target="_blank">
           <img className=" h-8 w-8" src="/Footer/mail.svg" alt="" />
-        </a>
+        </a> */}
         <a href={LINKS.gitHub} target="_blank">
           <img className=" h-8 w-8" src="/Footer/github.svg" alt="" />
         </a>
         <a href={LINKS.linkedIn} target="_blank">
-          <img className=" h-8 w-8" src="/Footer/linkedin.svg" alt="" />
+          <img className=" h-9 w-9" src="/Footer/linkedin.svg" alt="" />
         </a>
       </div>
-      <div className=" justify-self-center mb-6">
-        {languaje.home.calltoaction1}
-      </div>
+        <div className=" justify-self-center mb-6">
+      <a href={languaje.home.cvPath} download={languaje.home.cvName}>
+          {languaje.home.calltoaction1}
+      </a>
+        </div>
     </div>
   );
 };
